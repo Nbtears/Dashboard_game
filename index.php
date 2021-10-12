c820e63ea87fc9905a192d2ebbda12f6e3a0e1f0
Diana Aguilar <dianaleticia2001@hotmail.com>
2021-10-12 18:11:12 -0500

    footer y tabla

--- index.php
+++ index.php
@@ -1,6 +1,43 @@
 <?php include 'header.php'; ?>
+<?php include 'footer.php'; ?>
+<?php
+    include_once "conexion.php";
+    $temp = new Connection();
+    $conn = $temp->getConnection();
+    $sql = "SELECT * FROM user";
+    $result = $conn ->query($sql);
+    $row = $result->fetch_assoc();
+?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  </body>
-</html>
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md">
+            <div class="card-header">
+                Pacientes
+            </div>
+            <div class="p-4">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col-3">Apellidos</th>
+                        <th scope="col-3">Nombre(s)</th>
+                        <th scope="col-2">Diagnóstico</th>
+                        <th scope="col-2">Clínica</th>
+                        <th scope="col-1">Edad</th>
+                        <th scope="col-1">More</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td scope="row">Aguilar Jiménez</td>
+                        <td>Diana Leticia</td>
+                        <td>Fractura</td>
+                        <td>San José</td>
+                        <td>20</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>

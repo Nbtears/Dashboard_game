c9475186f3fe0d4aa86a0ff0b302758e239c1718
Diana Aguilar <dianaleticia2001@hotmail.com>
2021-10-12 18:30:21 -0500

    Tabla con info de base

--- index.php
+++ index.php
@@ -12,29 +12,36 @@
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md">
-            <div class="card-header">
-                Pacientes
+            <div class="card-header text-center">
+                Lista de pacientes
             </div>
             <div class="p-4">
                 <table class="table">
                     <thead>
                         <tr>
-                        <th scope="col-3">Apellidos</th>
-                        <th scope="col-3">Nombre(s)</th>
-                        <th scope="col-2">Diagnóstico</th>
-                        <th scope="col-2">Clínica</th>
-                        <th scope="col-1">Edad</th>
-                        <th scope="col-1">More</th>
+                            <th scope="col-3">Apellidos</th>
+                            <th scope="col-3">Nombre(s)</th>
+                            <th scope="col-2">Diagnóstico</th>
+                            <th scope="col-2">Clínica</th>
+                            <th scope="col-1">Edad</th>
+                            <th scope="col-1">More</th>
                         </tr>
                     </thead>
                     <tbody>
+
+                        <?php
+                            foreach($result as $row){
+                        ?>
                         <tr>
-                        <td scope="row">Aguilar Jiménez</td>
-                        <td>Diana Leticia</td>
-                        <td>Fractura</td>
-                        <td>San José</td>
-                        <td>20</td>
+                            <td scope="row"><?php echo $row["lastname"]; ?></td>
+                            <td><?php echo $row["name"]; ?></td>
+                            <td><?php echo $row["injury"]; ?></td>
+                            <td><?php echo  $row["clinic"]; ?></td>
+                            <td><?php echo  $row["age"]; ?></td>
                         </tr>
+                        <?php
+                            }
+                        ?>
                     </tbody>
                 </table>
             </div>
